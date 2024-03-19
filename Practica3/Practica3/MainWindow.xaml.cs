10473fc329eb864e4e5f20c50894d64177aa84a4
Maggitarius <144776072+Maggitarius@users.noreply.github.com>
2024-03-19 15:55:01 +0300

    Add files via upload

---
+++ Practica3/Practica3/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Practica3.DataSetATableAdapters;
+
+namespace Practica3
+{
+    public partial class MainWindow : Window
+    {
+        BooksTableAdapter books = new BooksTableAdapter();
+        AuthorsTableAdapter authors = new AuthorsTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Table.ItemsSource = authors.GetDataBy();
+        }
+
+        private void Ivent(object sender, EventArgs e)
+        {
+            Table.Columns[0].Visibility = Visibility.Collapsed;
+            Table.Columns[6].Visibility = Visibility.Collapsed;
+            Table.Columns[11].Visibility = Visibility.Collapsed;
+        }
+    }
+}
